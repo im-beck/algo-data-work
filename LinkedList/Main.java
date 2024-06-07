ac0148ff81205460f58056bc718593ad4a293d51
Beck <rebecca.pond@keyin.com>
2024-06-07 13:57:08 -0230

    completed singlelinkedlist assignment and fixed the null exception in main

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -7,7 +7,7 @@ public class Main {
         singleLinkedList.insertInLinkedList(2, 1);
         singleLinkedList.insertInLinkedList(3, 2);
         singleLinkedList.insertInLinkedList(4, 3);
-//        singleLinkedList.insertInLinkedList(5, 1);
+       singleLinkedList.insertInLinkedList(5, 1);
         singleLinkedList.traverseLinkedList();
         singleLinkedList.searchNode(4);
     }
@@ -17,3 +17,8 @@ public class Main {
 //Assignment question number
 //1. Uncomment line 10 "singleLinkedList.insertInLinkedList(5, 1)" and Investigate and fix
     // why when inserting a value at a position that is already occupied, it throws a null pointer exception.
+
+    // The NullPointerException was happening because the code was incorrectly linking the new node to itself instead of inserting it properly into the list. 
+    // This caused the list's structure to become invalid, leading to errors when the code tried to traverse or manipulate the list. 
+    // The correct approach ensures that the new node is linked between the correct existing nodes, maintaining the list's integrity and 
+    // avoiding the null reference errors.
--- LinkedList/SingleLinkedList.java
+++ LinkedList/SingleLinkedList.java
@@ -21,31 +21,33 @@ public class SingleLinkedList {
         //1. inserting at the begining
         //2. Inserting at the ending
         //3. Insert anywhere
-    public void insertInLinkedList(int nodeValue, int location){
-        Node node = new Node();
-        node.value = nodeValue;
-        if(head == null) {
-            createLinkedList(nodeValue);
-            return;
-        } else if (location == 0) {
-            node.next = head;
-            head = node;
-        } else if (location >= size) {
-            tail.next = node;
-            node.next = null;
-            tail = node;
-        } else {
-            Node tempNode = head;
-            int index = 0;
-            while (index < location - 1) {
-                tempNode = tempNode.next;
-                index++;
+        public void insertInLinkedList(int nodeValue, int location) {
+            Node node = new Node();
+            node.value = nodeValue;
+            if (head == null) {
+                createLinkedList(nodeValue);
+                return;
+            } else if (location == 0) {
+                node.next = head;
+                head = node;
+            } else if (location >= size) {
+                tail.next = node;
+                node.next = null;
+                tail = node;
+            } else {
+                Node tempNode = head;
+                int index = 0;
+                while (index < location - 1) {
+                    tempNode = tempNode.next;
+                    index++;
+                }
+                Node nextNode = tempNode.next;
+                tempNode.next = node;
+                node.next = nextNode;
             }
-            Node nextNode = node;
-            node.next = nextNode;
+            size++;
         }
-        size++;
-    }
+        
 
     //Traverse a Linked List
     public void traverseLinkedList(){
@@ -83,9 +85,42 @@ public class SingleLinkedList {
 
 
     //Deleting a Node from a Single linked List
-    //0. If link list doesn't
+    //0. If link list doesn't exist
     //1. deleting at the begining
     //2. deleting at the ending
     //3. deleting anywhere in the list
 
+    public void deleteNode(int location) {
+        if (head == null) {
+            System.out.println("The linked list does not exist");
+            return;
+        } else if (location == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) { 
+                tail = null;
+            }
+        } else if (location >= size - 1) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {  
+                head = tail = null;
+                size--;
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+        }
+
+    }
 }
