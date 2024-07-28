f6dbea642e710760584bbf0a34b43666b07455aa
Beck <rebecca.pond@keyin.com>
2024-07-28 18:18:24 -0230

    generated springboot project

---
+++ bstapplication/src/main/java/com/example/bstapplication/BstapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bstapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstapplicationApplication.class, args);
+	}
+
+}
